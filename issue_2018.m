a3bbdf11e367f6e59912f38dffee77790df2390f
pf3t3r <p.j.farrell@students.uu.nl>
2022-11-28 14:33:23 +0100

    checking M1...

--- issue_2018.m
+++ issue_2018.m
@@ -8,10 +8,11 @@ t1 = 33000;
 t2 = 37500;
 
 figure;
-subplot(2,2,1);plot(time(t1:t2),absoluteSalinity(1,t1:t2));title(depth_SA(1));
-subplot(2,2,2);plot(time(t1:t2),absoluteSalinity(2,t1:t2));title(depth_SA(2));
-subplot(2,2,3);plot(time(t1:t2),absoluteSalinity(3,t1:t2));title(depth_SA(3));
-subplot(2,2,4);plot(time(t1:t2),absoluteSalinity(4,t1:t2));title(depth_SA(4));
+subplot(2,2,1);plot(time(t1:t2),absoluteSalinity(1,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(1));
+subplot(2,2,2);plot(time(t1:t2),absoluteSalinity(2,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(2));
+subplot(2,2,3);plot(time(t1:t2),absoluteSalinity(3,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(3));
+subplot(2,2,4);plot(time(t1:t2),absoluteSalinity(4,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(4));
+
 
 % SUBPLOT 1
 % before July 2019 = level 5; after July 2018 = level 19
@@ -26,10 +27,10 @@ subplot(2,2,4);plot(time(t1:t2),absoluteSalinity(4,t1:t2));title(depth_SA(4));
 % before July 2018 = level 147, after July 2018 = level 156
 
 figure;
-subplot(2,2,1);plot(time(t1:t2),conservativeTemperature(1,t1:t2));title(depth_CT(1));
-subplot(2,2,2);plot(time(t1:t2),conservativeTemperature(2,t1:t2));title(depth_CT(2));
-subplot(2,2,3);plot(time(t1:t2),conservativeTemperature(3,t1:t2));title(depth_CT(3));
-subplot(2,2,4);plot(time(t1:t2),conservativeTemperature(4,t1:t2));title(depth_CT(4));
+subplot(2,2,1);plot(time(t1:t2),conservativeTemperature(1,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_CT(1));
+subplot(2,2,2);plot(time(t1:t2),conservativeTemperature(2,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_CT(2));
+subplot(2,2,3);plot(time(t1:t2),conservativeTemperature(3,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_CT(3));
+subplot(2,2,4);plot(time(t1:t2),conservativeTemperature(4,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_CT(4));
 
 % IC3: four sensors at the following depth levels
 % 1: 5,9 --->> no change July 2018
@@ -46,3 +47,39 @@ hold on
 plot(time,squeeze(T(4,:,48)));
 plot(time,squeeze(T(4,:,49)));
 hold off
+
+%%
+clc; close all; clear;
+load('Matfiles/M1.mat','time');
+load('Matfiles/SA_M1.mat');
+load('Matfiles/CT_M1.mat');
+depth_SA = load('Matfiles/depthsS.mat','depths_M1').depths_M1;
+depth_CT = load('Matfiles/depthsT.mat','depths_T_M1').depths_T_M1();
+t1 = 33000;
+t2 = 37500;
+
+figure;
+subplot(2,5,1);plot(time(t1:t2),absoluteSalinity(1,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(1));
+subplot(2,5,2);plot(time(t1:t2),absoluteSalinity(2,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(2));
+subplot(2,5,3);plot(time(t1:t2),absoluteSalinity(3,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(3));
+subplot(2,5,4);plot(time(t1:t2),absoluteSalinity(4,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(4));
+subplot(2,5,5);plot(time(t1:t2),absoluteSalinity(5,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(5));
+subplot(2,5,6);plot(time(t1:t2),absoluteSalinity(6,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(6));
+subplot(2,5,7);plot(time(t1:t2),absoluteSalinity(7,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(7));
+subplot(2,5,8);plot(time(t1:t2),absoluteSalinity(8,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(8));
+subplot(2,5,9);plot(time(t1:t2),absoluteSalinity(9,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(9));
+subplot(2,5,10);plot(time(t1:t2),absoluteSalinity(10,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(10));
+
+
+figure;
+subplot(2,5,1);plot(time(t1:t2),conservativeTemperature(1,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(1));
+subplot(2,5,2);plot(time(t1:t2),conservativeTemperature(2,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(2));
+subplot(2,5,3);plot(time(t1:t2),conservativeTemperature(3,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(3));
+subplot(2,5,4);plot(time(t1:t2),conservativeTemperature(4,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(4));
+subplot(2,5,5);plot(time(t1:t2),conservativeTemperature(5,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(5));
+subplot(2,5,6);plot(time(t1:t2),conservativeTemperature(6,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(6));
+subplot(2,5,7);plot(time(t1:t2),conservativeTemperature(7,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(7));
+subplot(2,5,8);plot(time(t1:t2),conservativeTemperature(8,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(8));
+subplot(2,5,9);plot(time(t1:t2),conservativeTemperature(9,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(9));
+subplot(2,5,10);plot(time(t1:t2),conservativeTemperature(10,t1:t2)); xline(time(35185)); xline(time(35209)); title(depth_SA(10));
+
